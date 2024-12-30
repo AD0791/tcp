ddb1694912bcb70d1dc567d613c22ef4c80ca473
Alexandro Disla <32604741+AD0791@users.noreply.github.com>
2024-12-30 12:02:11 -0500

    moving towards the finish line

--- tcp.go
+++ tcp.go
@@ -2,7 +2,32 @@ package main
 
 import (
 	"fmt"
+	"net"
 )
+
+type Server struct {
+	listenAddr string
+	listener   net.Listener
+	quitCh     chan struct{}
+}
+
+func NewTCPServer(address string) *Server {
+	return &Server{
+		listenAddr: address,
+	}
+}
+
+func (s *Server) Start() error {
+	ln, err := net.Listen("tcp", s.listenAddr)
+	if err != nil {
+		return err
+	}
+	defer ln.Close()
+	s.listener = ln
+
+	<-s.quitCh
+	return nil
+}
 
 func main() {
 	fmt.Println("tcp server")
